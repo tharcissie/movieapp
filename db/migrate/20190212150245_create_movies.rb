2c2778544bff3256aa37cb85da62054229aac756
tharcissie <tharcissieidufashe@gmail.com>
2019-02-24 02:16:22 -0800

    commit

--- db/migrate/20190212150245_create_movies.rb
+++ db/migrate/20190212150245_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
       t.string :title
-      t.integer : :year
+      t.integer :year
       t.string :actors
       t.integer :category
       t.decimal :price
